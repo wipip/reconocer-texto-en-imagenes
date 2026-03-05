5735c19267dfd7adabc555bef979a8b6ddb7525d
wipip <166628878+wipip@users.noreply.github.com>
2026-03-05 07:50:07 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -5,9 +5,9 @@ import pytesseract
 from PIL import Image
 
 
-st.title("Reconocimiento óptico de Caracteres")
+st.title("Reconocimiento de texto en imagenes")
 
-img_file_buffer = st.camera_input("Toma una Foto")
+img_file_buffer = st.camera_input("enfoca el texto a reconocer")
 
 with st.sidebar:
       filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
